b013fdc71cae5db586046b6db3c7dad623a53f9e
Sarah Rabah <sarahmrabah@gmail.com>
2019-08-28 14:49:06 +0200

    Moving files to api folder in backend

--- app/project/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from project.api.views import AllArtistsView
-
-app_name = 'api'
-
-urlpatterns = [
-    path('artist/', AllArtistsView.as_view(), name='artist_list'),
-]
