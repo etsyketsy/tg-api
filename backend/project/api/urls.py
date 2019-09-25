b1b7fb2a98396d1b1df747062d1a71a122303246
Sarah Rabah <sarahmrabah@gmail.com>
2019-09-25 14:52:31 +0200

    Adding CreateArtist endpoint - Endpoint manually tested with Postman

--- backend/project/api/urls.py
+++ backend/project/api/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from project.api.views import AllArtistsView, AllReleasesView
+from project.api.views import (
+    AllArtistsView,
+    CreateArtistView, 
+    AllReleasesView,
+)
+    
 
 app_name = 'api'
 
 urlpatterns = [
     path('artist/', AllArtistsView.as_view(), name='artist_list'),
-    path('release/', AllReleasesView.as_view(), name='release_list')
+    path('artist/new/', CreateArtistView.as_view(), name='new_artist'),
+
+    path('release/', AllReleasesView.as_view(), name='release_list'),
 ]
