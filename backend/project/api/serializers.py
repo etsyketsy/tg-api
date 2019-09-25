890f817d255dc9aafe7c3b793f312f9bbe976728
Sarah Rabah <sarahmrabah@gmail.com>
2019-09-25 13:00:42 +0200

    Adding in All Releases view

--- backend/project/api/serializers.py
+++ backend/project/api/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from .models import Artist
+from .models import Artist, Release
 
 
 class ArtistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Artist
         fields = ['name', 'tag']
+
+class ReleaseSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Release
+        fields = ['name', 'artist', 'release_number']
--- backend/project/api/urls.py
+++ backend/project/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from project.api.views import AllArtistsView
+from project.api.views import AllArtistsView, AllReleasesView
 
 app_name = 'api'
 
 urlpatterns = [
     path('artist/', AllArtistsView.as_view(), name='artist_list'),
+    path('release/', AllReleasesView.as_view(), name='release_list')
 ]
