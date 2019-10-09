d7da11f436b0d785a4c9c667dfb9cbeac29d28af
Sarah Rabah <sarahmrabah@gmail.com>
2019-10-09 11:38:45 +0200

    Adding image field to serializers

--- project/api/serializers.py
+++ project/api/serializers.py
@@ -5,11 +5,11 @@ from .models import Artist, Release
 class ArtistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Artist
-        fields = ['artist', 'artist_nice_name', 'artist_location', 'artist_bio','artist_type', 'artist_contact', 'status','id' ]
+        fields = ['artist', 'artist_nice_name', 'artist_location', 'artist_bio','artist_type', 'artist_contact', 'status','id', 'image' ]
 
 
 
 class ReleaseSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Release
-        fields = ['row', 'cat_num', 'fk_artist', 'release_title', 'release_formats', 'release_date', 'artist_nice_name', 'tracklisting', 'bio', 'ffo', 'target_markets', 'upc', 'status', 'mediaplayer_html', 'artist']
+        fields = ['row', 'cat_num', 'fk_artist', 'release_title', 'release_formats', 'release_date', 'artist_nice_name', 'tracklisting', 'bio', 'ffo', 'target_markets', 'upc', 'status', 'mediaplayer_html', 'artist', 'image']
