45cef4355f739200310368dd2148a1654b3af7c4
Sarah Rabah <sarahmrabah@gmail.com>
2019-10-24 07:29:29 -0700

    Removing excess models from admin.py and database

--- project/api/admin.py
+++ project/api/admin.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
-from .models import Artist, ArtistLink, Release, ReleaseLink, IncomeSource, Vendor, Income, Expense, Product, ProductVariant
-
+from .models import Artist, ArtistLink, Release, ReleaseLink
 
 admin.site.register(Artist)
 admin.site.register(ArtistLink)
 admin.site.register(Release)
 admin.site.register(ReleaseLink)
-admin.site.register(Vendor)
-admin.site.register(IncomeSource)
-admin.site.register(Income)
-admin.site.register(Expense)
-admin.site.register(Product)
-admin.site.register(ProductVariant)
