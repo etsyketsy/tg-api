ead819c033dbe18925b19333cefbf27d989148e1
Sarah Rabah <sarahmrabah@gmail.com>
2019-11-04 14:17:44 +0100

    Cleaning up url names to comply with RESTful concepts > - Reference model fields with plural

--- project/api/urls.py
+++ project/api/urls.py
@@ -14,24 +14,24 @@ from project.api.views import (
 app_name = 'api'
 
 urlpatterns = [
-    path('artist/', AllArtistsView.as_view(), name='artist_list'),
-    path('artist/new/', CreateArtistView.as_view(), name='new_artist'),
+    path('artists/', AllArtistsView.as_view(), name='artist_list'),
+    path('artists/new/', CreateArtistView.as_view(), name='new_artist'),
     path(
-        'artist/check/', 
+        'artists/check/', 
         AllArtistsNiceNameView.as_view(), 
         name='all_artist_nice_name'
     ),
 
     path(
-        'artist/<str:artist_nice_name>/', 
+        'artists/<str:artist_nice_name>/', 
         ArtistByNiceNameView.as_view(), 
         name='artist_by_nice_name'
     ),
 
-    path('release/', AllReleasesView.as_view(), name='release_list'),
-    path('release/new/', CreateReleaseView.as_view(), name='new_release'),
+    path('releases/', AllReleasesView.as_view(), name='release_list'),
+    path('releases/new/', CreateReleaseView.as_view(), name='new_release'),
     path(
-        'release/<str:cat_num>/', 
+        'releases/<str:cat_num>/', 
         ReleaseByCatNumView.as_view(), 
         name='release_by_id'
     ),
